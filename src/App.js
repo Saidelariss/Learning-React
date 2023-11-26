b4a4c4260d1a45eebe3ce9afa7f0a6a3604dc551
said el ariss <saidariss3@gmail.com>
2023-11-26 10:34:45 +0100

    first step in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+
+import { useState } from 'react';
+
+
+
+// des buttons indépendants 
+// export default function MyApp(){
+//   return (
+//     <div>
+//     <h1>des compteurs indépendants</h1>
+//     <MyButton />
+//     <MyButton />
+//     </div>
+//     );
+
+// }
+
+// function MyButton(){
+//   const [count,setCount] = useState(0); // 0 est la valeur initiale de count (hooks)
+//   function handleClick(){
+//     setCount(count+1)
+//   }
+//   return(
+//     <button onClick={handleClick}>cliqué {count} fois</button>
+//   )
+// }
+
+//Des compteurs synchronisés : On utilise les props, c'est-à-dire, on passe des informations provenant du composant parent
+export default function MyApp(){
+  const [count,setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Des compteurs synchronisés</h1>
+      <MyButton compteur={count} incrementCount={handleClick} />
+      <MyButton compteur={count} incrementCount={handleClick} />
     </div>
   );
 }
 
-export default App;
+function MyButton({compteur,incrementCount}){ // compteur,incrementCount sont des props
+  return(
+    <button onClick={incrementCount}>cliqué {compteur} fois</button>
+  )
+}
