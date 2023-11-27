6fa5829995625af9ab8ba12078c4b4d7b648c836
said el ariss <saidariss3@gmail.com>
2023-11-27 08:30:32 +0100

    add Tic-Tac-Toe game

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 
@@ -29,24 +29,52 @@ import { useState } from 'react';
 // }
 
 //Des compteurs synchronisés : On utilise les props, c'est-à-dire, on passe des informations provenant du composant parent
-export default function MyApp(){
-  const [count,setCount] = useState(0);
+// export default function MyApp(){
+//   const [count,setCount] = useState(0);
 
-  function handleClick(){
-    setCount(count+1)
-  }
+//   function handleClick(){
+//     setCount(count+1)
+//   }
 
+//   return (
+//     <div>
+//       <h1>Des compteurs synchronisés</h1>
+//       <MyButton compteur={count} incrementCount={handleClick} />
+//       <MyButton compteur={count} incrementCount={handleClick} />
+//     </div>
+//   );
+// }
+
+// function MyButton({compteur,incrementCount}){ // compteur,incrementCount sont des props
+//   return(
+//     <button onClick={incrementCount}>cliqué {compteur} fois</button>
+//   )
+// }
+
+//jeu Tic-Tac-Toe
+
+import { useState } from 'react';
+
+// Les composants React doivent ne renvoyer qu’un unique élément JSX, et non plusieurs éléments JSX adjacents, 
+export default function Board() {
   return (
-    <div>
-      <h1>Des compteurs synchronisés</h1>
-      <MyButton compteur={count} incrementCount={handleClick} />
-      <MyButton compteur={count} incrementCount={handleClick} />
-    </div>
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
   );
 }
 
-function MyButton({compteur,incrementCount}){ // compteur,incrementCount sont des props
-  return(
-    <button onClick={incrementCount}>cliqué {compteur} fois</button>
-  )
-}
